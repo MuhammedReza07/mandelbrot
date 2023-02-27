099e9b42fe3a55e9359aea42990494e1f54039a5
Muhammed Reza <mmr2000707@gmail.com>
2023-02-27 23:30:54 +0100

    Initialised by creating an iteration test.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use 
+
+fn main() {
+    let c = Complex {real: 0.0, imaginary: 0.0};
+    let mut s = Complex{real: 0.0, imaginary: 0.0};
+    for _ in 0..1000 {
+        s = s * s + c;
+    }
+    if s.abs().is_finite() {
+        println!("{} is in the Mandelbrot set.", c);
+    } else {
+        println!("{} is not in the Mandelbrot set.", c);
+    }
+}
